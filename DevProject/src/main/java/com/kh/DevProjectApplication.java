876fbcc0478ddf0a21e1551d46944b07941d8727
kyuhon <kyuhon123@gmail.com>
2025-01-06 15:08:22 +0900

    [create] springBoot 시작

---
+++ DevProject/src/main/java/com/kh/DevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication	//불러와서 객체화
+public class DevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProjectApplication.class, args);
+	}
+
+}
---
+++ DevProject/src/test/java/LomboksTests/LomboksTests.java
@@ -0,0 +1,5 @@
+package LomboksTests;
+
+public class LomboksTests {
+
+}
